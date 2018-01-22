3091e8752dbb10336aab3ed35e19e69625b00228
Maor Korakin <maor.korakin@gmail.com>
2018-01-21 18:08:28 -0800

    Removed comment

--- app/src/main/java/com/example/mkorakin/UiDesignPatternsByExample/Model/Model.kt
+++ app/src/main/java/com/example/mkorakin/UiDesignPatternsByExample/Model/Model.kt
@@ -2,11 +2,6 @@ package com.example.mkorakin.UiDesignPatternsByExample.Model
 
 import io.reactivex.Observable
 
-/**
- * An interface representing the Application Model.
- * It is implemented by a simple [InMemoryModel] that maintains its state in memory.
- * It can later be replaced with other implementations without modifying the rest of the app.
- */
 interface Model {
 
     val count : Observable<Int>
