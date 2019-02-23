deb1255a80460bef6c72927be42b752852aaf29e
Maor Korakin <maor.korakin@gmail.com>
2019-02-22 20:20:52 -0800

    fix build

--- app/src/main/java/com/example/mkorakin/UiDesignPatternsByExample/Model/InMemoryModel.kt
+++ app/src/main/java/com/example/mkorakin/UiDesignPatternsByExample/Model/InMemoryModel.kt
@@ -7,7 +7,7 @@ internal object InMemoryModel : Model {
     override val count = BehaviorSubject.createDefault(0)
 
     @Synchronized override fun incrementCount() {
-        count.onNext(count.value + 1)
+        count.onNext(count.value?.let { it + 1 } ?: 0)
     }
 
     @Synchronized override fun setCount(count: Int) {
